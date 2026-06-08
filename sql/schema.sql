36cf8f7021e96546bf24938b674144d7a8642aa7
KAES27 <srkadra@gmail.com>
2026-06-08 11:36:21 +0200

    Initialisation du projet

---
+++ sql/schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE project(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    owner_id INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE project_members (
+    project_id INT NOT NULL,
+    user_id INT NOT NULL,
+    role VARCHAR(50) DEFAULT 'member',
+    PRIMARY KEY (project_id, user_id),
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+CREATE TABLE task(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id NOT NULL,
+    assigned_to INT NULL,
+    title VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    is_done BOOLEAN DEFAULT FALSE,
+    priority VARCHAR(50) DEFAULT 'medium',
+    due_date DATE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+    FOREIGN KEY (assigned_to) REFERENCES users(id) ON DELETE SET NULL
+);
+CREATE TABLE comments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    task_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (task_id) REFERENCES task(id) ON DELETE CASCADE
+)
